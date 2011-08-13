6218300ac1cd3322c8c751669496b1e75604d003
DeathShadow <clancc@shadowkitsune.net>
2011-08-13 01:28:58 -0700

    more fixes to bds sync function

--- plugins/extensions/System.php
+++ plugins/extensions/System.php
@@ -762,9 +762,8 @@ class System_commands extends extension {
 						$info2 = explode(':', $info[0]);
 						$user = strtolower($info2[3]);
 						if($user == strtolower($this->Bot->username) && $this->dAmn->chat[$ns]['member'][$from]['pc'] == 'PoliceBot' && $from != $this->Bot->username) {
-							$bot=strtolower($this->Bot->username);
-							$user=strtolower($from);
-							$paa=$this->dAmn->format_chat('@'.$bot, $user);
+							$bot=$this->Bot->username;
+							$paa=$this->dAmn->format_chat('@'.$bot, $from);
 							$this->dAmn->join($paa);
 							$this->dAmn->npmsg($ns, "BDS:LINK:ACCEPT:{$from}", TRUE);
 						}elseif($user == strtolower($this->Bot->username) && $this->dAmn->chat[$ns]['member'][$from]['pc'] != 'PoliceBot')
@@ -775,9 +774,8 @@ class System_commands extends extension {
 						$info2 = explode(':', $info[0]);
 						$user = strtolower($info2[3]);
 						if($user == strtolower($this->Bot->username) && $this->dAmn->chat[$ns]['member'][$from]['pc'] == 'PoliceBot' && $from != $this->Bot->username) {
-							$bot=strtolower($this->Bot->username);
-							$user=strtolower($from);
-							$paa=$this->dAmn->format_chat('@'.$bot, $user);
+							$bot=$this->Bot->username;
+							$paa=$this->dAmn->format_chat('@'.$bot, $from);
 							$this->dAmn->join($paa);
 							sleep(1);
 							$this->dAmn->npmsg($paa, 'BDS:SYNC:BEGIN');
@@ -788,8 +786,8 @@ class System_commands extends extension {
 						$info2 = explode(':', $info[0]);
 						$user = strtolower($info2[3]);
 						if($user == strtolower($this->Bot->username) && $from != $this->Bot->username) {
-							$user=strtolower($from);
-							$paa=$this->dAmn->format_chat('@'.$bot, $user);
+							$bot=$this->Bot->username;
+							$paa=$this->dAmn->format_chat('@'.$bot, $from);
 							$this->dAmn->part($paa);
 						}
 					break;
