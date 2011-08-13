f7ad1d19e5aca7696fc5e4bbc83038a61c7aface
DeathShadow <clancc@shadowkitsune.net>
2011-08-13 15:49:10 -0700

    fixed botname: botcheck so it only responds to 'botname: botcheck' only and not 'botname: botcheck thing'

--- plugins/extensions/dAmn.php
+++ plugins/extensions/dAmn.php
@@ -193,7 +193,7 @@ class dAmn_commands extends extension {
 	}
 
 	function e_respond($ns, $from, $message, $target) {
-		if(stristr($message, $this->Bot->username.': botcheck') && $ns == 'chat:Botdom' || stristr($message, '<abbr title="'.$this->Bot->username.': botcheck"></abbr>') && stristr($message, $this->Bot->username) && $ns == 'chat:Botdom')
+		if($message == $this->Bot->username.': botcheck' && $ns == 'chat:Botdom' || stristr($message, '<abbr title="'.$this->Bot->username.': botcheck"></abbr>') && stristr($message, $this->Bot->username) && $ns == 'chat:Botdom')
 			$this->dAmn->say($ns, '<abbr title="away"></abbr>I\'m a bot. <abbr title="botresponse: '.$from.' '.$this->Bot->owner.' '.$this->Bot->info['name'].' '.$this->Bot->info['version'].'/'.$this->Bot->info['bdsversion'].' '.md5(strtolower(str_replace(' ', '', $this->Bot->trigger).$from.$this->Bot->username)).' '.$this->Bot->trigger.'"></abbr>');
 		if($ns == 'chat:Botdom' && $message == 'bots: botcheck')
 			$this->dAmn->npmsg('chat:datashare', 'BDS:BOTCHECK:RESPONSE:'.$from.','.$this->Bot->owner.','.$this->Bot->info['name'].','.$this->Bot->info['version'].'/'.$this->Bot->info['bdsversion'].','.md5(strtolower(str_replace(' ', '', $this->Bot->trigger).$from.$this->Bot->username)).','.$this->Bot->trigger, TRUE);
