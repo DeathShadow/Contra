7f88c63ead1205e8f5f2385069105663b683e912
DeathShadow <clancc@shadowkitsune.net>
2011-08-24 20:49:02 -0700

    [BDS] changed spammy default botinfo message to be less spammy.

--- plugins/extensions/System.php
+++ plugins/extensions/System.php
@@ -436,9 +436,7 @@ class System_commands extends extension {
 			}
 		}else{
 			$sb = "";
-			$this->dAmn->send("pong\n\0");
-			$sb .= "[<b>:dev" . implode( array_keys( $this->botdata ), ":</b>], [<b>:dev" ) . ":</b>]";
-			$this->dAmn->say( $ns, "<abbr title=\"{$from}\"> </abbr> You must specify the name of a bot you wish to get information for.<br><sub><sub>Currently known bots: {$sb}</sub></sub>" );
+			$this->dAmn->say($ns, "<abbr title=\"{$from}\"></abbr> You must specify the name of a bot you wish to get information for.<br /><sup>[There are ".count($this->botdata)." bots in database.]</sup>", TRUE);
 		}
 	}
 	function BDSBotCheck($ns, $sender, $payload) {
