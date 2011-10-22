edac21ecb1c0138d547449c28ef7929039e359d8
DeathShadow <clancc@shadowkitsune.net>
2011-10-21 22:57:48 -0700

    updated CODS:VERSION:NOTIFY

--- plugins/extensions/System.php
+++ plugins/extensions/System.php
@@ -841,15 +841,21 @@ class System_commands extends extension {
 					$version = $command2[1];
 					$released = $command2[2];
 					$reason = $command2[3];
-					if($user == $this->Bot->username) {
+					if(stristr($user, $this->Bot->username) && !stristr($user, 'ALL')) {
 						if(empty($user) || empty($version) || empty($released)) return;
 						if($version > $this->Bot->info['version'] && $from == 'Asuos') {
 							$this->Console->Alert("Contra {$version} has been released on {$released}. Get it at http://botdom.com/wiki/Contra#Latest");
 							if(!empty($reason)) $this->Console->Alert($reason);
 						}
 					}
+					if(stristr($user, 'ALL')) {
+						if(empty($version) || empty($released)) return;
+						if($version > $this->Bot->info['version'] && $from == 'Asuos') {
+							$this->Console->Alert("Contra {$version} has been released on {$released}. Get it at http://botdom.com/wiki/Contra#Latest");
+							if(!empty($reason)) $this->Console->Alert($reason);
+						}
+					}
 					break;
-				}
 			}
 		}
 	}
