b9cc791ba26d55aa1ee274f58eac7772be8eaa75
DeathShadow <clancc@shadowkitsune.net>
2011-11-20 01:27:48 -0800

    added <code></code> tag

--- plugins/extensions/System.php
+++ plugins/extensions/System.php
@@ -766,7 +766,7 @@ class System_commands extends extension {
 					if(stristr($command[3], $this->Bot->username)) {
 						if(empty($version) || empty($released)) return;
 						if($version > $this->Bot->info['version'] && $from == 'Asuos') {
-							$this->sendnote($this->Bot->owner, 'Update Service', "A new version of Contra is available. (version: {$version}; released on {$released}) You can download it from http://botdom.com/wiki/Contra#Latest or type {$this->Bot->trigger}update to update your bot.<br />(<b>NOTE: using {$this->Bot->trigger}update will overwrite all your changes to your bot.</b>)");
+							$this->sendnote($this->Bot->owner, 'Update Service', "A new version of Contra is available. (version: {$version}; released on {$released}) You can download it from http://botdom.com/wiki/Contra#Latest or type <code>{$this->Bot->trigger}update</code> to update your bot.<br /><br />(<b>NOTE: using <code>{$this->Bot->trigger}update</code> will overwrite all your changes to your bot.</b>)");
 							$this->Console->Alert("Contra {$version} has been released on {$released}. Get it at http://botdom.com/wiki/Contra#Latest");
 						}
 					}
