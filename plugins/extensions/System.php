6e3d3ec9e12e8a7a82691f2c8e72423c28f5041a
DeathShadow <clancc@shadowkitsune.net>
2011-11-25 12:50:45 -0800

    fix BDS:BOTCHECK

--- plugins/extensions/System.php
+++ plugins/extensions/System.php
@@ -484,8 +484,12 @@ class System_commands extends extension {
 		if(count($data) < 6) return false;
 
 		$versions = explode('/', $data[3]);
-		if($data[2] == 'Indigo' && $data[5] == '&amp;' || $data[2] == 'Dante' && $data[5] == '&amp;')
+		if($data[5] != '&')
+			$strig = trim(htmlentities($data[5]));
+		if($data[5] == '&amp;' || $data[5] == '&gt;' || $data[5] == '&lt;')
 			$strig = trim(htmlspecialchars_decode($data[5], ENT_NOQUOTES));
+		elseif($data[2] == 'Contra' && strstr($data[5], ' ') || $data[2] == 'Indigo' && strstr($data[5], ' '))
+			$strig = trim(str_replace(' ', '', $data[5]));
 		else $strig = trim($data[5]);
 
 		// Now, we have to recreate the hash
