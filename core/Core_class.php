28a5192098e22708fc076f5599a09d4323b18f4b
DeathShadow <clancc@shadowkitsune.net>
2011-12-06 16:45:00 -0800

    removed undefined $UTS.

--- core/Core_class.php
+++ core/Core_class.php
@@ -53,7 +53,7 @@ class Bot {
 		global $Bot;
 		$Bot = $this;
 		// Generate a session ID code.
-		if(DEBUG) $this->session = sha1($UTS+microtime());
+		if(DEBUG) $this->session = sha1(microtime());
 		// Our start time is here.
 		$this->start = time();
 		// System information string.
