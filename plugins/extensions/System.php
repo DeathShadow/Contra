72776602b871616c71428c4e0cf4e3eb0f781cf5
DeathShadow <deathshadow--666@shadowkitsune.net>
2012-03-02 23:08:16 -0800

    added CODS:VERSION:NOTIFY:ALL to allow mass notifications to all (outdated) Contras note: Contra from 5.4.5 only accepts notifications from the Bot on Botdom account.

--- plugins/extensions/System.php
+++ plugins/extensions/System.php
@@ -855,8 +855,8 @@ class System_commands extends extension {
 					$command2 = explode(',', $message, 5);
 					$version = $command2[1];
 					$released = $command2[2];
-					if(stristr($command[3], $this->Bot->username)) {
-						if(empty($version) || empty($released)) return;
+					if(empty($version) || empty($released)) return;
+					if(stristr($command2[0], $this->Bot->username) || strstr($command2[0], 'ALL')) {
 						if($version > $this->Bot->info['version'] && $from == 'Botdom') {
 							$this->botversion['latest'] = false;
 							$this->sendnote($this->Bot->owner, 'Update Service', "A new version of Contra is available. (version: {$version}; released on {$released}) You can download it from http://botdom.com/wiki/Contra#Latest or type <code>{$this->Bot->trigger}update</code> to update your bot.<br /><br />(<b>NOTE: using <code>{$this->Bot->trigger}update</code> will overwrite all your changes to your bot.</b>)");
