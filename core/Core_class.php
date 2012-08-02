521521bdca11d63fa834097d0781f96ac1b84f6d
DeathShadow <deathshadow--666@shadowkitsune.net>
2012-08-02 03:37:12 -0700

    now uses old authtoken from pre-5.5.0 if authtoken exists. But will still require oauth auth if the authtoken expires.

--- core/Core_class.php
+++ core/Core_class.php
@@ -128,6 +128,8 @@ class Bot {
 		$this->trigger = $config['info']['trigger'];
 		$this->aboutStr = $config['about'];
 		$this->autojoin = $config['autojoin'];
+		if(isset($config['cookie']))
+			$this->damntoken = unserialize($config['cookie']);
 		$this->damntoken = empty($config['damntoken']) ? '' : unserialize($config['damntoken']);
 		$this->updatenotes = empty($config['updatenotes']) ? true : $config['updatenotes'];
 	}
