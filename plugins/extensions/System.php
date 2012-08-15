ade8dd5ddfbc6cb28166d5696d0e6be692853a86
DeathShadow <deathshadow--666@shadowkitsune.net>
2012-08-15 13:57:37 -0700

    Remove DS check for CODS:BOTCHECK:ALL.

--- plugins/extensions/System.php
+++ plugins/extensions/System.php
@@ -538,7 +538,6 @@ class System_commands extends extension {
 			if(!strstr(strtolower($parts[3]), strtolower($this->Bot->username))) return;
 		if($parts[2] === 'ALL')
 			if($this->dAmn->chat['chat:DataShare']['member'][$from]['pc'] !== 'PoliceBot') return;
-			if($from !== 'Botdom') return;
 		if($parts[2] === 'NODATA')
 			if($this->dAmn->chat['chat:DataShare']['member'][$from]['pc'] !== 'PoliceBot') return;
 			if(strtolower($parts[3]) !== strtolower($this->Bot->username)) return;
