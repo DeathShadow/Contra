2d0da359b5bdc062f86158044d1c59f99c42da0a
Kyogo <eittreim.justin@live.com>
2012-10-26 18:06:29 -0700

    Fix issue #14 - Added notice comment for the future

--- core/Core_class.php
+++ core/Core_class.php
@@ -126,7 +126,8 @@ class Bot {
 		$config = include './storage/config.cf';
 		$this->username = $config['info']['username'];
 		$this->owner = $config['info']['owner'];
-		$this->trigger = mb_convert_encoding($config['info']['trigger'], 'HTML-ENTITIES', 'UTF-8');;
+		// Note: This may require the PHP extension 'mbstring', someone should look into this on Windows.
+		$this->trigger = mb_convert_encoding($config['info']['trigger'], 'HTML-ENTITIES', 'UTF-8');
 		$this->aboutStr = $config['about'];
 		$this->autojoin = $config['autojoin'];
 		if(isset($config['cookie']))
