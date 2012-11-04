b8b505cbe1da3333f25f327848fa7d24f776aef5
DeathShadow <deathshadow--666@shadowkitsune.net>
2012-11-04 13:50:40 -0800

    Fix formatting for autojoin list.

--- plugins/startup/dAmn_library.php
+++ plugins/startup/dAmn_library.php
@@ -156,7 +156,7 @@ class dAmn_lib extends extension {
 			while($i > 0) {
 				unset($ajn[array_search(strtolower($ns), $ajn)]);
 				sort($ajn);
-				$this->Bot->autojoin = $ajn;
+				$this->Bot->autojoin = $this->dAmn->deform_chat($ajn);
 				$i--;
 				$this->Bot->save_config();
 				$this->dAmn->part($ns);
