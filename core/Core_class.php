4b6d574966e9b44ea92b5682e227a7f5e8d5b7fe
DeathShadow <deathshadow--666@shadowkitsune.net>
2013-02-02 13:31:06 -0800

    Begin 5.5.10

--- core/Core_class.php
+++ core/Core_class.php
@@ -19,7 +19,7 @@ class Bot {
 	public $start;
 	public $info = array(
 		'name' => 'Contra',
-		'version' => '5.5.9',
+		'version' => '5.5.10',
 		'status' => '',
 		'release' => 'public',
 		'author' => 'photofroggy',
