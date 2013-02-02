65cdfe15e54085ee0fdda1acbf9465304f86219e
DeathShadow <deathshadow--666@shadowkitsune.net>
2013-02-02 13:40:48 -0800

    5.6.0 because my update notification system doesn't like 5.5.10.

--- core/Core_class.php
+++ core/Core_class.php
@@ -19,7 +19,7 @@ class Bot {
 	public $start;
 	public $info = array(
 		'name' => 'Contra',
-		'version' => '5.5.10',
+		'version' => '5.6.0',
 		'status' => '',
 		'release' => 'public',
 		'author' => 'photofroggy',
